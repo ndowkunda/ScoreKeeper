61594f0c11deb64893db219dbc16245d243de1a6
ndowkunda <ndowkunda@users.noreply.github.com>
2023-03-09 19:50:29 +0000

    add tests and implement scoring one point for team A

---
+++ java/junit5/src/main/java/ScoreKeeper.java
@@ -0,0 +1,21 @@
+public class ScoreKeeper {
+
+    private String score = "000:000";
+
+    public String getScore() {
+        return score;
+    }
+
+    public void scoreTeamA1() {
+        String scoreTeamA = score.substring(0, 3);
+        int scoreTeamAInt = Integer.parseInt(scoreTeamA);
+        int scoreLimit = 999;
+        if(scoreTeamAInt < scoreLimit) {
+            scoreTeamAInt++;
+        }
+        String scoreTeamAStr = String.format("%03d",scoreTeamAInt);
+        String scoreTeamBStr = score.substring(3);
+        score = scoreTeamAStr + scoreTeamBStr;
+    }
+
+}
---
+++ java/junit5/src/test/java/ScoreKeeperTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class ScoreKeeperTest {
+
+    @Test
+    void shouldReturnStartingScoreOfZeroForTeamAAndB() {
+        ScoreKeeper scoreKeeper = new ScoreKeeper();
+        String actualScore = scoreKeeper.getScore();
+        String expectedScore = "000:000";
+        assertThat(actualScore)
+                .isEqualTo(expectedScore);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1,001:000", "10,010:000", "100,100:000", "1000,999:000"})
+    void shouldReturnCorrectScoreForTeamA(int increment, String expectedScore) {
+        ScoreKeeper scoreKeeper = new ScoreKeeper();
+        for (int i = 0; i < increment; i++) {
+            scoreKeeper.scoreTeamA1();
+        }
+        String actualScore = scoreKeeper.getScore();
+        assertThat(actualScore).isEqualTo(expectedScore);
+    }
+}
