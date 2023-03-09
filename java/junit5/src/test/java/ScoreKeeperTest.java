267a3839d7354a08f3f1aed4eb255b4d74f9981a
ndowkunda <ndowkunda@users.noreply.github.com>
2023-03-09 20:10:35 +0000

    add tests and implement scoring one point for both teams

--- java/junit5/src/test/java/ScoreKeeperTest.java
+++ java/junit5/src/test/java/ScoreKeeperTest.java
@@ -18,10 +18,32 @@ class ScoreKeeperTest {
 
     @ParameterizedTest
     @CsvSource({"1,001:000", "10,010:000", "100,100:000", "1000,999:000"})
-    void shouldReturnCorrectScoreForTeamA(int increment, String expectedScore) {
+    void shouldReturnCorrectScoreForTeamAAfterOnePoint(int increment, String expectedScore) {
         ScoreKeeper scoreKeeper = new ScoreKeeper();
         for (int i = 0; i < increment; i++) {
             scoreKeeper.scoreTeamA1();
+        }
+        String actualScore = scoreKeeper.getScore();
+        assertThat(actualScore).isEqualTo(expectedScore);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1,000:001", "10,000:010", "100,000:100", "1000,000:999"})
+    void shouldReturnCorrectScoreForTeamBAfterOnePoint(int increment, String expectedScore) {
+        ScoreKeeper scoreKeeper = new ScoreKeeper();
+        for (int i = 0; i < increment; i++) {
+            scoreKeeper.scoreTeamB1();
+        }
+        String actualScore = scoreKeeper.getScore();
+        assertThat(actualScore).isEqualTo(expectedScore);
+    }
+    @ParameterizedTest
+    @CsvSource({"1,001:001", "10,010:010", "100,100:100", "1000,999:999"})
+    void shouldReturnCorrectScoreForBothTeamsAfterOnePointAdded(int increment, String expectedScore) {
+        ScoreKeeper scoreKeeper = new ScoreKeeper();
+        for (int i = 0; i < increment; i++) {
+            scoreKeeper.scoreTeamA1();
+            scoreKeeper.scoreTeamB1();
         }
         String actualScore = scoreKeeper.getScore();
         assertThat(actualScore).isEqualTo(expectedScore);
